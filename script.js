064c49eff7b41de8ba5ea7756d91e650f044fbd6
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2024-11-28 14:56:37 +0100

    feat: add getHumanChoice function
    Add a function to prompt the user for "rock", "paper", or "scissors".

--- script.js
+++ script.js
@@ -13,5 +13,23 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    while (true) {
+        let userInput = prompt("Choose: Rock, Paper, Scissors ?");
+        if (!userInput) {
+            alert("Input cannot be empty");
+            continue;
+        }
 
+        userInput = userInput.toLowerCase();
+
+        if (userInput === "rock" ||  userInput === "paper" || userInput === "scissors") {
+            return userInput;
+        } else {
+            alert("Invalid choice! Please choose Rock, Paper, or Scissors.");
+        }
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
