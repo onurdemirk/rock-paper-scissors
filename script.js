7f14d322f2ce82599a9f4e35db0b71c90c8d12d7
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2024-12-03 12:40:08 +0100

    Add new improvements and features for game functionality
    - Remove `humanChoice` function as prompt is now obsolete
    - Add `eventListener` to each button to capture `humanChoice`
    - Manipulate the DOM to display `computerChoiceImg`, `computerChoiceText`, and `gameScore` dynamically in each round
    - Implement `disableButton` function to stop the game when a player reaches 5 points
    - Add `enableButton` function to restart the game when the user clicks
    - Create `resetGame` function to clear all round data and reset the game state
    - Add `resetButton` functionality to trigger `resetGame` and `enableButtons` for a fresh start

--- script.js
+++ script.js
@@ -1,72 +1,142 @@
 function getComputerChoice() {
-    let value = Math.floor(Math.random() * 3);
-    switch(value) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";   
-    }
+  let value = Math.floor(Math.random() * 3);
+  switch (value) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
 }
 
-function getHumanChoice() {
-    while (true) {
-        let userInput = prompt("Choose: Rock, Paper, Scissors ?");
-        if (!userInput) {
-            alert("Input cannot be empty");
-            continue;
-        }
+const computer = document.querySelector(".computer");
+const finalScore = document.querySelector("#finalScore");
+const computerChoiceImg = document.createElement("img");
+const computerChoiceText = document.createElement("p");
+const gameScore = document.createElement("p");
+const scoreMessage = document.querySelector("#scoreMessage");
 
-        userInput = userInput.toLowerCase();
+let disableButtons = () => {
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+};
 
-        if (userInput === "rock" ||  userInput === "paper" || userInput === "scissors") {
-            return userInput;
-        } else {
-            alert("Invalid choice! Please choose Rock, Paper, or Scissors.");
-        }
-    }
-}
+let enableButtons = () => {
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+};
 
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const humanChoice = button.id;
+    playGame(humanChoice);
+  });
+});
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a draw!");
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
-    }
+  if (humanChoice === computerChoice) {
+    computerChoiceImg.src = `/img/${computerChoice}.png`;
+    computerChoiceImg.alt = `${computerChoice}`;
+    computerChoiceText.textContent = `Computer chooses: ${computerChoice
+      .charAt(0)
+      .toUpperCase()}${computerChoice.slice(1)}`;
+    gameScore.textContent = "It's a draw";
+    finalScore.textContent = "";
+    computer.appendChild(finalScore);
+    computer.appendChild(computerChoiceImg);
+    computer.appendChild(computerChoiceText);
+    computer.appendChild(gameScore);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    computerChoiceImg.src = `/img/${computerChoice}.png`;
+    computerChoiceImg.alt = `${computerChoice}`;
+    computerChoiceText.textContent = `Computer chooses: ${computerChoice
+      .charAt(0)
+      .toUpperCase()}${computerChoice.slice(1)}`;
+    gameScore.textContent = `You win! ${humanChoice} beats ${computerChoice}, choose again!`;
+    computer.appendChild(computerChoiceImg);
+    computer.appendChild(computerChoiceText);
+    computer.appendChild(gameScore);
+    humanScore++;
+  } else {
+    computerChoiceImg.src = `/img/${computerChoice}.png`;
+    computerChoiceImg.alt = `${computerChoice}`;
+    computerChoiceText.textContent = `Computer chooses: ${computerChoice
+      .charAt(0)
+      .toUpperCase()}${computerChoice.slice(1)}`;
+    gameScore.textContent = `You lose! ${computerChoice} beats ${humanChoice}, choose again!`;
+    computer.appendChild(computerChoiceImg);
+    computer.appendChild(computerChoiceText);
+    computer.appendChild(gameScore);
+    computerScore++;
+  }
 
-    console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+  scoreMessage.textContent = `Your Score: ${humanScore} - Computer Score: ${computerScore}`;
 }
 
-function playGame() {
+function playGame(humanChoice) {
+  const computerSelection = getComputerChoice();
+  playRound(humanChoice, computerSelection);
 
-    while(humanScore < 5 && computerScore < 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-    
-        playRound(humanSelection, computerSelection);
-    }
+  if (humanScore === 5) {
+    finalScore.textContent = "CONGRATULATIONS! You Win!";
+    disableButtons();
 
-    if (humanScore === 5) {
-         console.log("CONGRATULATIONS! You Win!");
-    } else if (computerScore === 5) {
-         console.log("GAME OVER! Computer Wins!");
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Play again?";
+    resetButton.style.cursor = "pointer";
+    resetButton.addEventListener("click", resetGame);
+
+    computerChoiceImg.src = "";
+    computerChoiceImg.alt = "";
+    computerChoiceText.textContent = "";
+    gameScore.textContent = "";
+
+    computer.appendChild(finalScore);
+    computer.appendChild(resetButton);
+  } else if (computerScore === 5) {
+    finalScore.textContent = "GAME OVER! Computer Wins!";
+    disableButtons();
+
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Try again?";
+    resetButton.style.cursor = "pointer";
+    resetButton.addEventListener("click", resetGame);
+
+    computerChoiceImg.src = "";
+    computerChoiceImg.alt = "";
+    computerChoiceText.textContent = "";
+    gameScore.textContent = "";
+
+    computer.appendChild(finalScore);
+    computer.appendChild(resetButton);
+  }
 }
-}
 
-const button = document.getElementById("button");
-button.addEventListener("click", function() {
-    playGame()
-});
+let resetGame = (event) => {
+  humanScore = 0;
+  computerScore = 0;
+
+  computerChoiceImg.src = "";
+  computerChoiceImg.alt = "";
+  computerChoiceText.textContent = "";
+  gameScore.textContent = "";
+  finalScore.textContent = "";
+  scoreMessage.textContent = `Your Score: ${humanScore} - Computer Score: ${computerScore}`;
+
+  const resetButton = event.target;
+  resetButton.remove();
+
+  enableButtons();
+};
